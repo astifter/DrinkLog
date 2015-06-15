0237ab7fbb9970cf2b148bb46a0cf4ef65b883ab
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-06-15 14:20:37 +0200

    Disabled Settings menu for now.

--- app/src/main/java/com/astifter/drinklog/MainActivity.java
+++ app/src/main/java/com/astifter/drinklog/MainActivity.java
@@ -230,10 +230,10 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this, SettingsActivity.class);
-            startActivity(intent);
-        }
+        //if (id == R.id.action_settings) {
+        //    Intent intent = new Intent(this, SettingsActivity.class);
+        //    startActivity(intent);
+        //}
 
         return super.onOptionsItemSelected(item);
     }
